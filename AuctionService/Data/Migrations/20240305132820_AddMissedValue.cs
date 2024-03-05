04024e043cc245ecafff75dc5149e5e3471cadc2
manhattan <andrey2004uor@gmail.com>
2024-03-05 21:58:45 +0200

    first init

---
+++ AuctionService/Data/Migrations/20240305132820_AddMissedValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissedValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "imageUrl",
+                table: "Items",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AuctionEnd",
+                table: "Auctions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuctionEnd",
+                table: "Auctions");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Items",
+                newName: "imageUrl");
+        }
+    }
+}
